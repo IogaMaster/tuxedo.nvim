6a6377fb3fd1edf2861ebb706f28ec15afabc502
IogaMaster <iogamastercode@gmail.com>
2026-06-01 20:46:54 -0600

    deps: add dev tools

--- flake.nix
+++ flake.nix
@@ -5,38 +5,45 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = {nixpkgs, ...}: let
-    forAllSystems = function:
-      nixpkgs.lib.genAttrs [
-        "x86_64-linux"
-        "aarch64-linux"
-      ] (system: function nixpkgs.legacyPackages.${system});
-  in rec {
-    devShells = forAllSystems (pkgs: {
-      default = pkgs.mkShell {
-        nativeBuildInputs = with pkgs; [
-          alejandra
-        ];
-      };
-    });
+  outputs =
+    { nixpkgs, ... }:
+    let
+      forAllSystems =
+        function:
+        nixpkgs.lib.genAttrs [
+          "x86_64-linux"
+          "aarch64-linux"
+        ] (system: function nixpkgs.legacyPackages.${system});
+    in
+    rec {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            alejandra
 
-    # packages = forAllSystems (pkgs: {
-    #   mcman = pkgs.callPackage ./pkgs/mcman {};
-    #   playit = pkgs.callPackage ./pkgs/playit {};
-    # });
-    #
-    # nixosModules.flux = ./modules/flux;
-    # nixosModules.default = nixosModules.flux;
-    #
-    # overlays.default = final: prev: {
-    #   mcman = final.callPackage ./pkgs/mcman {};
-    #   playit = final.callPackage ./pkgs/playit {};
-    #
-    #   mkGenericServer = final.callPackage ./builders/mkGenericServer.nix {};
-    #   mkMinecraftServer = final.callPackage ./builders/mkMinecraftServer.nix {};
-    #   mkSteamServer = final.callPackage ./builders/mkSteamServer.nix {};
-    #
-    #   fetchSteam = final.callPackage ./helpers/fetchSteam {};
-    # };
-  };
+            stylua
+            lua-language-server
+          ];
+        };
+      });
+
+      # packages = forAllSystems (pkgs: {
+      #   mcman = pkgs.callPackage ./pkgs/mcman {};
+      #   playit = pkgs.callPackage ./pkgs/playit {};
+      # });
+      #
+      # nixosModules.flux = ./modules/flux;
+      # nixosModules.default = nixosModules.flux;
+      #
+      # overlays.default = final: prev: {
+      #   mcman = final.callPackage ./pkgs/mcman {};
+      #   playit = final.callPackage ./pkgs/playit {};
+      #
+      #   mkGenericServer = final.callPackage ./builders/mkGenericServer.nix {};
+      #   mkMinecraftServer = final.callPackage ./builders/mkMinecraftServer.nix {};
+      #   mkSteamServer = final.callPackage ./builders/mkSteamServer.nix {};
+      #
+      #   fetchSteam = final.callPackage ./helpers/fetchSteam {};
+      # };
+    };
 }
